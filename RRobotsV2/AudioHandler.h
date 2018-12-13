d9ef1b7325c536cb10cee4baa80d66050674055b
Fredrik Lindlöf <fredrik.lindlof@svenskaspel.se>
2018-12-13 17:07:46 +0100

    Syntax error

--- RRobotsV2/AudioHandler.h
+++ RRobotsV2/AudioHandler.h
@@ -6,7 +6,7 @@
 #include <SDL.h>
 #include <SDL_mixer.h>
 #endif
-#if defined __APPLE__ | defined __LINUX__
+#if defined __APPLE__ || defined __LINUX__
 #include <SDL2/SDL2.h>
 #include <SDL2/SDL_mixer.h>
 #endif

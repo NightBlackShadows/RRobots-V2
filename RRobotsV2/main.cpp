731a328c5bcd0731df816de4c02eb0f38cdd766e
NightBlackShadows <35600687+NightBlackShadows@users.noreply.github.com>
2018-12-30 14:27:40 +0100

    Last fix for if def

--- RRobotsV2/main.cpp
+++ RRobotsV2/main.cpp
@@ -4,7 +4,9 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #endif
-#if defined __APPLE__ || defined __linux__
+#ifdef __APPLE__ 
+#endif
+#ifdef __linux__
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #endif

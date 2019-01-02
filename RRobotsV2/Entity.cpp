296453eb8c5075d794ca58f6dc8a78d7ffa6faff
NightBlackShadows <35600687+NightBlackShadows@users.noreply.github.com>
2019-01-02 20:48:39 +0100

    Update Entity.cpp

--- RRobotsV2/Entity.cpp
+++ RRobotsV2/Entity.cpp
@@ -1,13 +1,14 @@
 #include "Entity.h"
 
 
-
+//Constructor of an entity
 Entity::Entity(std::string filepath)
 {
 	this->filepath = filepath;
 	this->tex = NULL;
 }
 
+//Constructor of an entity with width and height of the image
 Entity::Entity(std::string filepath, int width, int height)
 {
 	this->filepath = filepath;
@@ -16,43 +17,50 @@ Entity::Entity(std::string filepath, int width, int height)
 	position.h = height;
 }
 
+//Set the xpos of the enity
 void Entity::setMapX(int x)
 {
 	mapX = x;
 }
 
+//Set the ypos of the entity
 void Entity::setMapY(int y)
 {
 	mapY = y;
 }
 
+//Return the position of the entity
 SDL_Rect * Entity::getPositionRect()
 {
 	return &position;
 }
 
+//Get the texture of the entity
 SDL_Texture * Entity::getTexture()
 {
 	return tex;
 }
 
+//Get the map xpos of the entity
 int Entity::getMapXPos()
 {
 	return mapX;
 }
 
+//get the map ypos of the entity
 int Entity::getMapYPos()
 {
 	return mapY;
 }
 
+//Set a new position
 void Entity::setNewPosition(int x, int y)
 {
 	position.x = x;
 	position.y = y;
 }
 
-
+//Desturctor
 Entity::~Entity()
 {
 	SDL_DestroyTexture(tex);

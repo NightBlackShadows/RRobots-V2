894167cf72b9ae3ae59634481fe5732e5d766c43
Fredrik Lindlöf <fredrik.lindlof@svenskaspel.se>
2019-02-25 11:26:49 +0100

    refactor filehandler.cpp

--- RRobotsV2/FileHandler.cpp
+++ RRobotsV2/FileHandler.cpp
@@ -10,38 +10,39 @@ FileHandler::FileHandler()
 //Check if file excists
 bool FileHandler::fileExicts(const std::string& name)
 {
-	struct stat buffer;
-	return (stat(name.c_str(), &buffer) == 0);
+  struct stat buffer;
+  return (stat(name.c_str(), &buffer) == 0);
 }
 
 //Write file
 bool FileHandler::writeToFile(std::string filename, std::string text)
 {
-	std::fstream fileStream;
-	fileStream.open(filename, std::ios::out);
+  std::fstream fileStream;
+  fileStream.open(filename, std::ios::out);
 	
-	if (fileStream.is_open()) {
-		fileStream << text;
-		fileStream.close();
-		return true;
-	}
-	return false;
+  if (fileStream.is_open())
+    {
+      fileStream << text;
+      fileStream.close();
+      return true;
+    }
+  return false;
 }
 
 /*
-std::fstream FileHandler::readFromFile(std::string filename)
-{
-	std::fstream fileStream;
-	std::string shit;
+  std::fstream FileHandler::readFromFile(std::string filename)
+  {
+  std::fstream fileStream;
+  std::string shit;
 
-	fileStream.open(filename, std::ios::in);
+  fileStream.open(filename, std::ios::in);
 
-	if (fileStream.is_open())
-	{
-		return fileStream;
-	}
-	else throw ("Unable to open file:" + filename);
-}*/
+  if (fileStream.is_open())
+  {
+  return fileStream;
+  }
+  else throw ("Unable to open file:" + filename);
+  }*/
 
 
 

2b23e517253e159f4d0bdea4eac64f4af5bf0f64
Fredrik Lindlöf <fredrik.lindlof@svenskaspel.se>
2019-02-25 11:17:16 +0100

    refactored audiohandler.cpp to be more readable.

--- RRobotsV2/AudioHandler.cpp
+++ RRobotsV2/AudioHandler.cpp
@@ -4,37 +4,37 @@
 //Constructor
 AudioHandler::AudioHandler()
 {
-	this->loaded = false;
-	this->frequency = 22050;
-	this->channels = 2;
-	this->chunksize = 4096;
-	this->format = MIX_DEFAULT_FORMAT;
+  this->loaded    = false;
+  this->frequency = 22050;
+  this->channels  = 2;
+  this->chunksize = 4096;
+  this->format    = MIX_DEFAULT_FORMAT;
 }
 
 
 
 bool AudioHandler::initSounds()
 {
-	//Initialize the music
-	if (SDL_Init(SDL_INIT_AUDIO) < 0)
-	{
-		printf("SDL could not init audio! SDL_Error: %s\n", SDL_GetError());
-		return false;
-	}
+  //Initialize the music
+  if (SDL_Init(SDL_INIT_AUDIO) < 0)
+    {
+      printf("SDL could not init audio! SDL_Error: %s\n", SDL_GetError());
+      return false;
+    }
 
-	if (Mix_OpenAudio(frequency, format, channels, chunksize) == -1)
-	{
-		printf("Could not init sdl mixer.\n");
-		return false;
-	}
-	return true;
+  if (Mix_OpenAudio(frequency, format, channels, chunksize) == -1)
+    {
+      printf("Could not init sdl mixer.\n");
+      return false;
+    }
+  return true;
 }
 
 // Load game sounds
 bool AudioHandler::loadSounds()
 {
 
-	return true;
+  return true;
 }
 
 //Play a specifik sound
@@ -50,7 +50,7 @@ void AudioHandler::pauseSound()
 //Change volume for sounds
 void AudioHandler::changeVolume(int volume)
 {
-	this->EffectVolume = volume;
+  this->EffectVolume = volume;
 }
 
 AudioHandler::~AudioHandler()
